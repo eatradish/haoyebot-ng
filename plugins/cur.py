7f73c65ef1d2b63890c6e2050e05b3acb750ab06
eatradish <qingbaodou@gmail.com>
2018-04-15 23:58:23 +0800

    fix logic error

--- plugins/cur.py
+++ plugins/cur.py
@@ -7,7 +7,7 @@ async def cur(bot, msg):
 	amount = lst[2]
 	headers = {'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,ja;q=0.6,zh-TW;q=0.5,uz;q=0.4,vi;q=0.3', 'Connection': 'keep-alive', 'Accept': '*/*', 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36', 'Accept-Encoding': 'gzip, deflate, br', 'Referer': 'http://www.cngold.org/fx/huansuan.html'}
 	async with aiohttp.ClientSession(headers = headers) as session:
-		async with sesson.get("https://api.jijinhao.com/plus/convert.htm?from_tkc=" + from_tkc + '&to_tkc=' + to_tkc + '&amount=' + amount) as resp:
+		async with session.get("https://api.jijinhao.com/plus/convert.htm?from_tkc=" + from_tkc + '&to_tkc=' + to_tkc + '&amount=' + amount) as resp:
 			result = resp.text
 			result = result.replace("var result = ", "")
 			result = result.replace("'", "")
