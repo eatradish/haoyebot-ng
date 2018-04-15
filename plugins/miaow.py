d16fe5abadc68632fff187ffd1e4ec8da3746718
eatradish <qingbaodou@gmail.com>
2018-04-16 07:37:19 +0800

    add miaow plugin

---
+++ plugins/miaow.py
@@ -0,0 +1,13 @@
+async def miaow(bot, msg):
+    dic = {'q': 'p', 'p': 'q', 'd': 'b', 'b': 'd'}
+    run = ['a', 'w', 'u']
+    lst = list(msg['text'])
+    for i in range(len(lst)):
+        if lst[i] in dic.keys():
+            lst[i] = dic[lst[i]]
+        lst = list(filter(lambda x: x in dic.keys() or x in run, lst))
+        while lst[0] in run:
+            lst = lst[1:]
+        while lst[-1] in run:
+            lst = lst[:-1]
+    await bot.sendMessage(msg['chat']['id'], "".join(lst))
